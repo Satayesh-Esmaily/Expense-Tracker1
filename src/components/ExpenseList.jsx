18e05c575de6ab39adcc0c14f7c48dc25d2b62b1
Satayesh Esmaily <setayeshesmaily59@gmail.com>
2026-01-07 12:48:39 +0430

    first commit

---
+++ src/components/ExpenseList.jsx
@@ -0,0 +1,20 @@
+import ExpenseItem from "./ExpenseItem";
+
+export default function ExpenseList({ expenses, onDeleteExpense, onEditExpense }) {
+  if (expenses.length === 0) {
+    return <p className="empty">No expenses yet. Add your first expense ✅</p>;
+  }
+
+  return (
+    <ul className="list">
+      {expenses.map((exp) => (
+        <ExpenseItem
+          key={exp.id}
+          exp={exp}
+          onDelete={onDeleteExpense}
+          onEdit={onEditExpense}
+        />
+      ))}
+    </ul>
+  );
+}
